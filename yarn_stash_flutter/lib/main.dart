c343612c887455025de2b56de07e44b56ba50dc0
spoppitty <sarahliu0127@gmail.com>
2026-06-17 18:28:21 -0700

    Basic Firebase Setup

--- yarn_stash_flutter/lib/main.dart
+++ yarn_stash_flutter/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app_style.dart';
+import 'firebase_options.dart';
 import 'screens.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: AppColors.bg,
